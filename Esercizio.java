bf50f185e360f488128061068e92f9ce139ebe9b
FNC-linoMaselli <andrea.maselli@marconibari.edu.it>
2024-11-29 09:57:10 +0000

    consegna

--- Esercizio.java
+++ Esercizio.java
@@ -1,26 +1,77 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
-
-//Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
 
-// Classe principale, con metodo main
-class Esercizio {
-    // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String[] V = new String[5];
+        String[] W = new String[5];
+        int[] X = new int[10];
+        int[] Y = new int[10];
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
+        V[0] = "Giuseppe Bono";
+        V[1] = "Symon c";
+        V[2] = "Lorenzo Jovanotti";
+        V[3] = "Tiziano Ferro";
+        V[4] = "Vasco Rossi";
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+        int i = 0;
+        while (i < 5) {
+            System.out.print("Inserire il nome del " + (i + 1) + " cantante: ");
+            W[i] = in.next();
+            i++;
+        }
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        i = 0;
+        while (i < 10) {
+            X[i] = i + 1;
+            i++;
+        }
+
+        i = 0;
+        while (i < 10) {
+            Y[i] = i * 2;
+            i++;
+        }
+
+        System.out.println("1° " + V[0] + " 2° " + V[1] + " 3° " + V[2]);
+
+        i = 0;
+        while (i < 5) {
+            System.out.println((i + 1) + "° elemento di V " + V[i]);
+            System.out.println((i + 1) + "° elemento di W " + W[i]);
+            i++;
+        }
+
+        i = 0;
+        while (i < 10) {
+            System.out.println((i + 1) + "° elemento di X " + X[i]);
+            System.out.println((i + 1) + "° elemento di Y " + Y[i]);
+            i++;
+        }
+
+        i = 0;
+        while (i < 10) {
+            if (i % 2 == 0) {
+                System.out.println(i + "° elemento pari " + X[i]);
+            }
+            i++;
+        }
+
+        i = 0;
+        while (i < 10) {
+            if (i % 2 != 0) {
+                System.out.println(i + "° Elemento dispari " + X[i]);
+            }
+            i++;
+        }
+
+        i = 0;
+        while (i < 10) {
+            if (X[i] % 2 == 0) {
+                System.out.println(X[i] + " è pari");
+            }
+            i++;
+        }
     }
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
